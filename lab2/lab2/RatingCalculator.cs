de88a205c7af0c1a9dc58ae14bd838db77689c27
Sviatozar <svpetrenko123@gmail.com>
2021-02-19 19:05:18 +0200

    Add Student class to RatingCalculator and make RatingCalculator static

--- lab2/lab2/RatingCalculator.cs
+++ lab2/lab2/RatingCalculator.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace lab2
 {
-    class RatingCalculator
+
+    public static class RatingCalculator
     {
+
+        private class Student
+        {
+            private string name;
+            private int[] grades;
+            private bool isContract;
+
+            public Student(string name, int[] grades, bool isContract)
+            {
+                this.name = name;
+                this.grades = grades;
+                this.isContract = isContract;
+            }
+        }
     }
 }
