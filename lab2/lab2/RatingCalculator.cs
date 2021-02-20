d05f51e01f2fde562e1dcf4c52c56523adec86c5
Sviatozar <svpetrenko123@gmail.com>
2021-02-20 22:41:40 +0200

    Separate Student from RatingCalculator and add functions to Student to convert string entries into Student objects

--- lab2/lab2/RatingCalculator.cs
+++ lab2/lab2/RatingCalculator.cs
@@ -9,29 +9,9 @@ namespace lab2
 
     public static class RatingCalculator
     {
-
-        private class Student
+        public static void CompileRating()
         {
-            private string name;
-            private int[] grades;
-            private bool isContract;
-
-            public Student(string name, int[] grades, bool isContract)
-            {
-                this.name = name;
-                this.grades = grades;
-                this.isContract = isContract;
-            }
-
-            public float GetAverage()
-            {
-                int total = 0;
-                foreach (int grade in grades)
-                {
-                    total += grade;
-                }
-                return (float) total / grades.Length;
-            }
+            // TODO
         }
     }
 }
---
+++ lab2/lab2/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Student
+    {
+        private string name;
+        private int[] grades;
+        private bool isContract;
+
+        // accepts an list of strings in the following format: "Ivanov,78,61,95,87,90,FALSE"
+        public static List<Student> GenerateStudentList(List<string> records)
+        {
+            List<Student> result = new List<Student>();
+            foreach (string record in records)
+            {
+                result.Add(new Student(record));
+            }
+            return result;
+        }
+        public Student(string name, int[] grades, bool isContract)
+        {
+            this.name = name;
+            this.grades = grades;
+            this.isContract = isContract;
+        }
+
+        // accepts a string in the following format: "Ivanov,78,61,95,87,90,FALSE"
+        public Student(string record)
+        {
+            string[] split = record.Split(',');
+            int[] grades = new int[split.Length - 2];
+            for (int i = 1; i < split.Length - 1; i++)
+            {
+                grades[i - 1] = Convert.ToInt32(split[i]);
+            }
+            bool isContract = split[split.Length - 1] == "FALSE" ? false : true;
+            this.name = split[0];
+            this.grades = grades;
+            this.isContract = isContract;
+        }
+
+        
+
+        public float GetAverage()
+        {
+            int total = 0;
+            foreach (int grade in grades)
+            {
+                total += grade;
+            }
+            return (float)total / grades.Length;
+        }
+    }
+}
